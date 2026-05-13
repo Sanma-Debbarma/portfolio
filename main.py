3f3f01871108def73280f8d737d874534eec582d
Sanma-Debbarma <sanmadebbarma22@gmail.com>
2026-05-13 14:10:57 +0530

    contact

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+app.config['MONGO_URL'] = "mongodb+srv://lala:lalakicho@cluster0.1exk2c0.mongodb.net/"
+client = MongoClient(app.config['MONGO_URL'])
+db = client["contact_db"]
+collection = db["contacts"]
 
 @app.route('/')
 def index():
@@ -14,10 +19,23 @@ def about():
 def projects():
     return render_template('index.html', section='projects')
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        name = request.form.GET("name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        message = request.form.get("message")
+
+        send = {
+            "name": name,
+            "email": email,
+            "phone": phone,
+            "message": message
+        }
+        .insert_one(send)
+
     return render_template('index.html', section='contact')
 
 if __name__ == '__main__':
       app.run(host="0.0.0.0", port=10000)
-
